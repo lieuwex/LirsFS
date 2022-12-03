b0b4cc2ad14a1d5c400a0c67d0139626b9767cfe
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2022-12-03 19:30:23 +0100

    config: cleanup default_file_registry function

--- dfs/src/config.rs
+++ dfs/src/config.rs
@@ -5,21 +5,20 @@ use async_raft::NodeId;
 use serde::{Deserialize, Serialize};
 use tokio::time::Duration;
 
+const fn default_file_registry() -> Cow<'static, str> {
+    if cfg!(debug_assertions) {
+        Cow::Borrowed("sqlite:///tmp/db/dev.db")
+    } else {
+        Cow::Borrowed("sqlite:///local/ddps2221/fileregistry.db")
+    }
+}
+
 const fn default_ping_interval() -> Duration {
     Duration::from_secs(60)
 }
 
 const fn default_max_missed_pings() -> usize {
     2
-}
-
-#[cfg(debug_assertions)]
-const fn default_file_registry() -> Cow<'static, str> {
-    Cow::Borrowed("sqlite:///tmp/db/dev.db")
-}
-#[cfg(not(debug_assertions))]
-const fn default_file_registry() -> Cow<'static, str> {
-    Cow::Borrowed("sqlite:///local/ddps2221/fileregistry.db")
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -34,10 +33,11 @@ pub struct Config {
     pub nodes: Vec<Node>,
     pub listen_port: usize,
 
+    #[serde(default = "default_file_registry")]
+    pub file_registry: Cow<'static, str>,
+
     #[serde(default = "default_ping_interval")]
     pub ping_interval: Duration,
     #[serde(default = "default_max_missed_pings")]
     pub max_missed_pings: usize,
-    #[serde(default = "default_file_registry")]
-    pub file_registry: Cow<'static, str>,
 }
