960b2dbd72d368706488e39169ef3bec2d5e4cd3
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2022-12-08 16:24:39 +0100

    assume_client: timeout if acquiring client takes too long

--- dfs/src/network.rs
+++ dfs/src/network.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 use anyhow::{anyhow, Result};
 use async_raft::{
@@ -12,6 +12,8 @@ use async_raft::{
 use tarpc::context;
 
 use crate::{client_req::AppClientRequest, connection::NodeConnection, CONFIG};
+
+pub const CLIENT_ACQUIRE_TIMEOUT: Duration = Duration::from_secs(1);
 
 /// A type which emulates a network transport and implements the `RaftNetwork` trait.
 pub struct AppRaftNetwork {
@@ -37,11 +39,20 @@ impl AppRaftNetwork {
 
 #[macro_export]
 macro_rules! assume_client {
-    ($network:expr, $node_id:expr) => {{
+    ($network:expr, $node_id:expr, $timeout:expr) => {{
         let node = $network.assume_node($node_id)?;
 
-        // TODO: timeout if it takes too long
-        node.get_client().await
+        let fut = node.get_client();
+        if let Some(timeout) = $timeout {
+            tokio::time::timeout(timeout, fut).await?
+        } else {
+            fut.await
+        }
+    }};
+
+    ($network:expr, $node_id:expr) => {{
+        use crate::network::CLIENT_ACQUIRE_TIMEOUT;
+        assume_client!($network, $node_id, Some(CLIENT_ACQUIRE_TIMEOUT))
     }};
 
     ($node_id:expr) => {{
@@ -58,7 +69,7 @@ impl RaftNetwork<AppClientRequest> for AppRaftNetwork {
         target: u64,
         rpc: AppendEntriesRequest<AppClientRequest>,
     ) -> Result<AppendEntriesResponse> {
-        let client = assume_client!(self, target);
+        let client = assume_client!(self, target, None);
         Ok(client.append_entries(context::current(), rpc).await?)
     }
 
@@ -68,13 +79,13 @@ impl RaftNetwork<AppClientRequest> for AppRaftNetwork {
         target: u64,
         rpc: InstallSnapshotRequest,
     ) -> Result<InstallSnapshotResponse> {
-        let client = assume_client!(self, target);
+        let client = assume_client!(self, target, None);
         Ok(client.install_snapshot(context::current(), rpc).await?)
     }
 
     /// Send a RequestVote RPC to the target Raft node (§5).
     async fn vote(&self, target: u64, rpc: VoteRequest) -> Result<VoteResponse> {
-        let client = assume_client!(self, target);
+        let client = assume_client!(self, target, None);
         Ok(client.vote(context::current(), rpc).await?)
     }
 }
