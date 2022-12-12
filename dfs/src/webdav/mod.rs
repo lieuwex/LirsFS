5780fb4f851f441416cab56aa9de8a884d328552
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2022-12-12 23:04:01 +0100

    webdav: raft.client_read check for every request

--- dfs/src/webdav/mod.rs
+++ dfs/src/webdav/mod.rs
@@ -13,11 +13,11 @@ mod seekfrom;
 use std::{convert::Infallible, net::SocketAddr};
 
 use anyhow::{anyhow, Result};
-use hyper::{Body, Request};
-use tokio::sync::OwnedRwLockReadGuard;
+use hyper::{Body, Request, Response};
+use tokio::{sync::OwnedRwLockReadGuard, time::Instant};
 use webdav_handler::{fakels::FakeLs, localfs::LocalFs, DavHandler};
 
-use crate::service::ServiceClient;
+use crate::{service::ServiceClient, RAFT};
 
 type Client = OwnedRwLockReadGuard<Option<ServiceClient>, ServiceClient>;
 
@@ -32,7 +32,29 @@ pub async fn listen(addr: &SocketAddr) -> Result<()> {
         async move {
             let func = move |req: Request<Body>| {
                 let dav_server = dav_server.clone();
-                async move { Ok::<_, Infallible>(dav_server.handle(req).await) }
+
+                async move {
+                    let raft = RAFT.get().unwrap();
+
+                    // (lieuwe): I have the feeling this is very slow, look at the source...
+                    // But to be sure I will keep it here and profile it.
+                    let start = Instant::now();
+                    let read = raft.client_read().await;
+                    eprintln!("raft.client_read() took {:?}", start.elapsed());
+
+                    let res = match read {
+                        Ok(_) => dav_server.handle(req).await,
+                        Err(e) => {
+                            eprintln!("catched raft error before webdav: {:?}", e);
+
+                            let mut resp = Response::builder();
+                            resp = resp.header("Content-Length", "0").status(500); // TODO
+                            resp = resp.header("connection", "close");
+                            resp.body(webdav_handler::body::Body::empty()).unwrap()
+                        }
+                    };
+                    Ok::<_, Infallible>(res)
+                }
             };
             Ok::<_, Infallible>(hyper::service::service_fn(func))
         }
