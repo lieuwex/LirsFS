b5b7fdf5108a7046ff5359c3aaf8088cc87790c4
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2022-12-13 23:42:13 +0100

    OutstandingWrites: get_all

--- dfs/src/db/outstanding_writes.rs
+++ dfs/src/db/outstanding_writes.rs
@@ -1,5 +1,7 @@
+use anyhow::Result;
+use futures::prelude::*;
 use serde::{Deserialize, Serialize};
-use sqlx::query;
+use sqlx::{query, sqlite::SqliteRow, Row, SqliteConnection};
 
 use super::schema::{Schema, SqlxQuery};
 
@@ -11,6 +13,27 @@ pub struct OutstandingWriteRow {
 }
 
 pub struct OutstandingWrites;
+
+impl OutstandingWrites {
+    pub async fn get_all(conn: &mut SqliteConnection) -> Result<Vec<OutstandingWriteRow>> {
+        let res = query("SELECT serial, file_id, node_id FROM outstanding_writes")
+            .fetch(conn)
+            .map(|r| anyhow::Ok(r?))
+            .and_then(|row: SqliteRow| async move {
+                Ok(OutstandingWriteRow {
+                    serial: {
+                        let val: i64 = row.get("serial");
+                        u64::try_from(val)?
+                    },
+                    file_id: row.get("file_id"),
+                    node_id: row.get("node_id"),
+                })
+            })
+            .try_collect()
+            .await?;
+        Ok(res)
+    }
+}
 
 impl Schema for OutstandingWrites {
     const TABLENAME: &'static str = "outstanding_writes";
