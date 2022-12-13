c234ea828a20857d80afbeae910fb9de87ffdf5b
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2022-12-13 23:53:57 +0100

    util: add blob_to_hash

--- dfs/src/util.rs
+++ dfs/src/util.rs
@@ -14,6 +14,19 @@ where
     }
 }
 
+pub fn blob_to_hash(hash: &[u8]) -> Result<u64> {
+    if hash.len() != 8 {
+        bail!(
+            "expected hash to be 8 bytes, but it is {} bytes",
+            hash.len()
+        );
+    }
+
+    let mut bytes: [u8; 8] = [0; 8];
+    bytes.copy_from_slice(&hash);
+    Ok(u64::from_be_bytes(bytes))
+}
+
 pub fn davpath_to_pathbuf(path: &DavPath) -> Utf8PathBuf {
     Utf8PathBuf::from_path_buf(path.as_pathbuf()).unwrap()
 }
