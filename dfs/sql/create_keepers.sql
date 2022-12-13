0ece3f0d4c9979010edb1c60d769922be1f1ce31
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2022-12-13 23:36:56 +0100

    keepers: also store latest known hash

--- dfs/sql/create_keepers.sql
+++ dfs/sql/create_keepers.sql
@@ -2,6 +2,11 @@ CREATE TABLE IF NOT EXISTS keepers (
     id          integer primary key,
     file_id     integer not null,
     node_id     integer not null,
+
+    -- latest calculated hash for this file for this keeper.
+    -- hash is in big-endian byte order.
+    hash                blob not null,
+
     FOREIGN KEY(node_id) REFERENCES node(id)
     FOREIGN KEY(file_id) REFERENCES file(id)
 );
