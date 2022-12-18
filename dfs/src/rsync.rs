70a40a652f8f2e9d310a8d36daa096015a0f905e
Sjors Holtrop <sjors@sholtrop.dev>
2022-12-18 15:12:21 +0100

    remove unnecessary rsync methods

--- dfs/src/rsync.rs
+++ dfs/src/rsync.rs
@@ -5,36 +5,24 @@ use tokio::process::Command;
 
 use crate::{util, CONFIG};
 
-pub struct Rsync {}
+pub async fn copy_from(node_id: NodeId, filename: &Utf8Path) -> Result<()> {
+    let remote_host = CONFIG
+        .get_node_ssh_host(node_id)
+        .ok_or_else(|| anyhow!("Node with id {:?} has no known socket address", node_id))?;
+    let full_path = util::prepend_fs_dir(filename);
 
-impl Rsync {
-    pub async fn read_from() -> Result<()> {
-        todo!()
-    }
-
-    pub async fn write_to() -> Result<()> {
-        todo!()
-    }
-
-    pub async fn copy_from(node_id: NodeId, filename: &Utf8Path) -> Result<()> {
-        let remote_host = CONFIG
-            .get_node_ssh_host(node_id)
-            .ok_or_else(|| anyhow!("Node with id {:?} has no known socket address", node_id))?;
-        let full_path = util::prepend_fs_dir(filename);
-
-        let output = Command::new("rsync")
-            .arg(format!("{remote_host}:{full_path}"))
-            .arg(full_path)
-            .output()
-            .await?;
-        if !output.status.success() {
-            // TODO: If we implement logging, log the stderr/stdout of `rsync` on error
-            Err(anyhow!(
-                "Rsync returned an error status code: {:#?}",
-                output.status.code()
-            ))
-        } else {
-            Ok(())
-        }
+    let output = Command::new("rsync")
+        .arg(format!("{remote_host}:{full_path}"))
+        .arg(full_path)
+        .output()
+        .await?;
+    if !output.status.success() {
+        // TODO: If we implement logging, log the stderr/stdout of `rsync` on error
+        Err(anyhow!(
+            "Rsync returned an error status code: {:#?}",
+            output.status.code()
+        ))
+    } else {
+        Ok(())
     }
 }
