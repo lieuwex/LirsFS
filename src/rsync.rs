e72136b1675605e12df01c29c71240ed7f856782
Sjors Holtrop <sjors@sholtrop.dev>
2022-12-20 17:42:41 +0100

    fix prepend_fs_dir interface, add args to `rsync` command

--- src/rsync.rs
+++ src/rsync.rs
@@ -6,15 +6,17 @@ use tokio::process::Command;
 use crate::{util, CONFIG};
 
 pub async fn copy_to(target_node: NodeId, filename: impl AsRef<Utf8Path>) -> Result<()> {
-    let remote_host = CONFIG.get_node_ssh_host(target_node).ok_or_else(|| {
-        anyhow!(
-            "Target node with id {:?} not found in config",
-            target_node
-        )
-    })?;
+    let filename = filename.as_ref();
+    let remote_host = CONFIG
+        .get_node_ssh_host(target_node)
+        .ok_or_else(|| anyhow!("Target node with id {:?} not found in config", target_node))?;
     let full_path = util::prepend_fs_dir(filename);
 
     let output = Command::new("rsync")
+        .arg("--compress")
+        .arg("--archive")
+        .arg("--checksum")
+        .arg("--preallocate")
         .arg(&full_path)
         .arg(format!("{remote_host}:{full_path}"))
         .output()
@@ -22,7 +24,10 @@ pub async fn copy_to(target_node: NodeId, filename: impl AsRef<Utf8Path>) -> Res
     if !output.status.success() {
         let error = String::from_utf8_lossy(&output.stderr);
         tracing::error!(
-            ?file_name, target_node, "Rsync returned an error copying file {filename} to node {target_node}: {error:#?}"
+            "Rsync returned an error copying file {:#?} to node {}: {:#?}",
+            filename,
+            target_node,
+            error
         );
         Err(anyhow!(
             "Rsync returned an error status code: {:#?}",
